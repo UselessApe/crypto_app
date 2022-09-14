d5533e15367e9895680775a1832371ed3f3ab53c
uselessApe <50899059+UselessApe@users.noreply.github.com>
2022-09-14 17:38:27 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:crypto_app/blocs/bloc/crypto_bloc.dart';
+import 'package:crypto_app/pages/home/home_page.dart';
+import 'package:crypto_app/services/data_repo.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider(
+      create: (context) => CryptoRepository(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Colors.blueGrey,
+          colorScheme:
+              const ColorScheme.dark().copyWith(secondary: Colors.white),
+        ),
+        home: BlocProvider(
+          create: (context) => CryptoBloc(
+            cryptoRepository: context.read<CryptoRepository>(),
+          )..add(Start()),
+          child: const HomePage(),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/model/data_error.dart
@@ -0,0 +1,11 @@
+import 'package:equatable/equatable.dart';
+
+class DataError extends Equatable {
+  final String message;
+  const DataError({
+    this.message = "",
+  });
+
+  @override
+  List<Object?> get props => [message];
+}
